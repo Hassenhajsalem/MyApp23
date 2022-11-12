c14493cbfed2735183e42c25630ad51722854a34
hassen haj salem <hajsalemhassen648@gmail.com>
2022-11-12 22:35:55 +0100

    commit

---
+++ App22/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
